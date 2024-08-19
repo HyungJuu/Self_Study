f26bb748ebdefef6302ddab0f59de3048b955b4f
HyungJuu <rmsdk617@naver.com>
2024-08-19 12:21:57 +0900

    .

---
+++ WPF/DataTemplateBasic/DataTemplateBasic/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private List<Student> students = default!;
+
+        public MainViewModel()
+        {
+            Students = Student.Students;
+            CurrentViewModel = new TestViewModel();
+        }
+
+        public List<Student> Students { get => students; set { students = value; } }
+        public ViewModelBase CurrentViewModel { get; set; }
+    }
+}
